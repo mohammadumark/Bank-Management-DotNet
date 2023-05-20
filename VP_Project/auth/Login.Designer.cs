19947c6bf9d72f5fc28eac64b9ac8ad1c95f8158
M Zubair Naeem <zubairnaeem45@gmail.com>
2023-05-21 00:48:07 +0500

    2 screen UIs

---
+++ VP_Project/auth/Login.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace VP_Project.auth
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.emailtextBox = new System.Windows.Forms.TextBox();
+            this.passtextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(208, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(563, 53);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bank Management System";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label2.Location = new System.Drawing.Point(350, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(281, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Login To Continue";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(313, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 33);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(313, 305);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 33);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.Info;
+            this.button1.Location = new System.Drawing.Point(313, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(379, 58);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "LOGIN";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // emailtextBox
+            // 
+            this.emailtextBox.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.emailtextBox.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.emailtextBox.Location = new System.Drawing.Point(442, 232);
+            this.emailtextBox.Name = "emailtextBox";
+            this.emailtextBox.Size = new System.Drawing.Size(250, 39);
+            this.emailtextBox.TabIndex = 9;
+            this.emailtextBox.Enter += new System.EventHandler(this.emailtextBox_Enter);
+            this.emailtextBox.Leave += new System.EventHandler(this.emailtextBox_Leave);
+            // 
+            // passtextBox
+            // 
+            this.passtextBox.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.passtextBox.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.passtextBox.Location = new System.Drawing.Point(442, 299);
+            this.passtextBox.Name = "passtextBox";
+            this.passtextBox.Size = new System.Drawing.Size(250, 39);
+            this.passtextBox.TabIndex = 10;
+            this.passtextBox.Enter += new System.EventHandler(this.passtextBox_Enter);
+            this.passtextBox.Leave += new System.EventHandler(this.passtextBox_Leave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label4.Location = new System.Drawing.Point(318, 475);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(228, 26);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Don\'t have an account?";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label6.Location = new System.Drawing.Point(543, 475);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Register Now";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.passtextBox);
+            this.Controls.Add(this.emailtextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Tag = "esfs";
+            this.Text = "Login";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.Enter += new System.EventHandler(this.Login_Enter);
+            this.Leave += new System.EventHandler(this.Login_Leave);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label5;
+        private Button button1;
+        private TextBox emailtextBox;
+        private TextBox passtextBox;
+        private Label label4;
+        private Label label6;
+    }
+}
---
+++ VP_Project/auth/Login.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VP_Project.auth
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            emailtextBox.Text = "Enter your email";
+            passtextBox.Text = "Enter your password";
+            emailtextBox.ForeColor = Color.Gray;
+            passtextBox.ForeColor = Color.Gray;
+        }
+
+        private void emailtextBox_Enter(object sender, EventArgs e)
+        {
+            if(emailtextBox.Text == "Enter your email")
+            {
+                emailtextBox.Text = "";
+                emailtextBox.ForeColor = Color.Black;
+            }
+        }
+
+        private void passtextBox_Enter(object sender, EventArgs e)
+        {
+            if(passtextBox.Text == "Enter your password")
+            {
+                passtextBox.Text = "";
+                passtextBox.ForeColor= Color.Black;
+            }
+        }
+
+        private void Login_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Leave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void emailtextBox_Leave(object sender, EventArgs e)
+        {
+            if (emailtextBox.Text == "")
+            {
+                emailtextBox.Text = "Enter your email";
+                emailtextBox.ForeColor = Color.Gray;
+            }
+        }
+
+        private void passtextBox_Leave(object sender, EventArgs e)
+        {
+            if (passtextBox.Text == "")
+            {
+                passtextBox.Text = "Enter your password";
+                passtextBox.ForeColor = Color.Gray;
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            Register register= new Register();
+            this.Hide();
+            register.Show();
+        }
+    }
+}
